d4fbcddd3dab7ed02ac25064f6a09791b87c34fc
bi-jvo <>
2018-12-27 11:22:18 +0100

    create repo + travis file

---
+++ build/precache-manifest.99074987241cfb297663bba5278ecf27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bac518422bd26872309e",
+    "url": "/static/css/main.7c49c16e.chunk.css"
+  },
+  {
+    "revision": "bac518422bd26872309e",
+    "url": "/static/js/main.bac51842.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "83c18309d92985b3e3277967df8ea6b7",
+    "url": "/index.html"
+  }
+];
